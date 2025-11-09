f2b20e77143b1ce07de557789e03c85dfad3e864
yuddev28 <yudcoder@gmail.com>
2025-11-09 13:16:58 +0700

    add method get all cinemas, get cinema by id, delete cinema by id

---
+++ MovieTicketBooking/src/main/java/model/dao/CinemaDao.java
@@ -0,0 +1,82 @@
+package model.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import model.bean.Cinema;
+
+public class CinemaDao {
+	// Get all cinema
+	public List<Cinema> getAllCinema() {
+		List<Cinema> list = new ArrayList<>();
+		try {
+			String query = "SELECT * FROM cinemas;";
+			// Create connect
+			Connection connect = JBDCConnection.getConnection();
+			Statement st = connect.createStatement();
+			ResultSet rs = st.executeQuery(query);
+			int id;
+			String name;
+			String address;
+			Cinema cinema;
+			while (rs.next()) {
+				id = rs.getInt("cinema_id");
+				name = rs.getString("cinema_name");
+				address = rs.getString("cinema_address");
+				cinema = new Cinema(id, name, address);
+				list.add(cinema);
+			}
+			connect.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return list;
+	}
+
+	// Get cinema by id
+	public Cinema getCinemaById(int id) {
+		Cinema cinema = null;
+		try {
+			String query = "SELECT * FROM cinemas WHERE cinema_id = ?;";
+			// Create connect
+			Connection connect = JBDCConnection.getConnection();
+			PreparedStatement st = connect.prepareStatement(query);
+			st.setInt(1, id);
+			ResultSet rs = st.executeQuery(query);
+			String name;
+			String address;
+			while (rs.next()) {
+				id = rs.getInt("cinema_id");
+				name = rs.getString("cinema_name");
+				address = rs.getString("cinema_address");
+				cinema = new Cinema(id, name, address);
+			}
+			connect.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return cinema;
+	}
+
+	// Delete cinema by id
+	public int deleteCinemaById(int id) {
+		int update = 0;
+		try {
+			String query = "DELETE FROM cinemas WHERE cinema_id = ?;";
+			// Create connect
+			Connection connect = JBDCConnection.getConnection();
+			PreparedStatement st = connect.prepareStatement(query);
+			st.setInt(1, id);
+			update = st.executeUpdate();
+			connect.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return update;
+	}
+}
