3f262cee3429cc7ba1f7ba4a62944d06a294dba5
haduydai <daithan348@gmail.com>
2026-01-01 23:35:38 +0700

    cập nhật mã hóa mật khẩu người dùng điền vào ở  logiiServlet để kiểm tra với mật khẩu được mã hóa dưới cơ sở dữ liệu

--- MovieTicketBooking/src/main/java/controller/LoginServlet.java
+++ MovieTicketBooking/src/main/java/controller/LoginServlet.java
@@ -2,6 +2,7 @@ package controller;
 
 import dao.UserDAO;
 import model.User;
+import utils.PasswordUtils;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -15,7 +16,7 @@ public class LoginServlet extends HttpServlet {
 	// GET: Để hiển thị trang đăng nhập
 	@Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        req.getRequestDispatcher("login.jsp").forward(req, resp);
+        req.getRequestDispatcher("/WEB-INF/view/login.jsp").forward(req, resp);
     }
 	// POST: Xử lý đăng nhập
     @Override
@@ -34,10 +35,12 @@ public class LoginServlet extends HttpServlet {
         if (user == null) {
             // Trường hợp: Tài khoản không tồn tại
             request.setAttribute("error", "Tài khoản không tồn tại. <a href='register.jsp'>Đăng ký ngay</a>");
-            request.getRequestDispatcher("login.jsp").forward(request, response);
+            request.getRequestDispatcher("/WEB-INF/view/login.jsp").forward(request, response);
         } else {
             // Trường hợp: Tài khoản có tồn tại, kiểm tra mật khẩu
-            if (user.getPassword().equals(pass)) {
+        	//mã hóa mật khẩu người dùng vừa nhập vào form để so sánh với dữ liệu mật khẩu được mã hóa dưới database
+            String inputHash	= PasswordUtils.hashPassword(pass);
+        	if (user.getPassword().equals(inputHash)) {
                 // Đăng nhập thành công -> Lưu vào Session
                 HttpSession session = request.getSession();
                 session.setAttribute("user", user); // Lưu đối tượng user để profile.jsp dùng
@@ -47,7 +50,7 @@ public class LoginServlet extends HttpServlet {
             } else {
                 // Sai mật khẩu
                 request.setAttribute("error", "Sai mật khẩu! Vui lòng thử lại.");
-                request.getRequestDispatcher("login.jsp").forward(request, response);
+                request.getRequestDispatcher("/WEB-INF/view/login.jsp").forward(request, response);
             }
         }
     }
