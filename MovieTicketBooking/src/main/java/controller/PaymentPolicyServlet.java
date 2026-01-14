62752c670d6f34621f611b117711a87dd4c8ac19
ha dai <daithan348@gmail.com>
2026-01-14 23:23:37 +0700

    thêm servlet cho paymnet-policy

---
+++ MovieTicketBooking/src/main/java/controller/PaymentPolicyServlet.java
@@ -0,0 +1,16 @@
+package controller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "PaymentPolicyServlet", urlPatterns = {"/payment-policy"})
+public class PaymentPolicyServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("/WEB-INF/view/payment-policy.jsp").forward(req, resp);
+    }
+}
