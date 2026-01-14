28f6bcba824968a340ac4d838391cc1a508f8ea7
ha dai <daithan348@gmail.com>
2026-01-14 23:25:05 +0700

    thêm servlet cho privacy

---
+++ MovieTicketBooking/src/main/java/controller/PrivacyServlet.java
@@ -0,0 +1,16 @@
+package controller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "PrivacyServlet", urlPatterns = {"/privacy"})
+public class PrivacyServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("/WEB-INF/view/privacy.jsp").forward(req, resp);
+    }
+}
