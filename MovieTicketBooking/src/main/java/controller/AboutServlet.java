7b549efd0ccf3c46bb734cf99475129d7847282f
ha dai <daithan348@gmail.com>
2026-01-14 23:14:51 +0700

    thêm servlet cho about

---
+++ MovieTicketBooking/src/main/java/controller/AboutServlet.java
@@ -0,0 +1,16 @@
+package controller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "AboutServlet", urlPatterns = {"/about"})
+public class AboutServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("/WEB-INF/view/about.jsp").forward(req, resp);
+    }
+}
