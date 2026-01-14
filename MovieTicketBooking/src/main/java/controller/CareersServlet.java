45e65414782723a3482f5840059a3445e1f420ae
ha dai <daithan348@gmail.com>
2026-01-14 23:16:31 +0700

    thêm servlet cho careers

---
+++ MovieTicketBooking/src/main/java/controller/CareersServlet.java
@@ -0,0 +1,16 @@
+package controller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "CareersServlet", urlPatterns = {"/careers"})
+public class CareersServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("/WEB-INF/view/careers.jsp").forward(req, resp);
+    }
+}
